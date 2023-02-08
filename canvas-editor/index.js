a22bdcf011c405d2471abf5c5c0916644f6dc8d4
wanglin2 <1013335014@qq.com>
2023-02-08 16:29:24 +0800

    编辑开发中

--- canvas-editor/index.js
+++ canvas-editor/index.js
@@ -21,8 +21,11 @@ class CanvasEditor {
     this.pageCanvasCtxList = [] // 页面canvas绘图上下文列表
     this.rows = [] // 渲染的行数据
     this.positionList = [] // 定位元素列表
+    this.cursorPositionIndex = -1 // 当前光标所在元素索引
     this.cursorEl = null // 光标元素
-    this.cursorTimer = null// 光标元素闪烁的定时器
+    this.cursorTimer = null // 光标元素闪烁的定时器
+    this.textareaEl = null // 文本输入框元素
+    this.isCompositing = false // 是否正在输入拼音
 
     this.createPage(0)
     this.render()
@@ -30,6 +33,9 @@ class CanvasEditor {
 
   // 渲染
   render() {
+    this.clear()
+    this.rows = []
+    this.positionList = []
     this.computeRows()
     this.renderPage()
   }
@@ -248,15 +254,9 @@ class CanvasEditor {
     let { x, y } = this.windowToCanvas(e, this.pageCanvasList[pageIndex])
     // 计算该坐标对应的元素索引
     let positionIndex = this.getPositionByPos(x, y, pageIndex)
-    // 根据元素索引计算出光标位置和高度信息
-    let cursorInfo = this.getCursorInfo(this.positionList[positionIndex])
-    // 渲染光标
-    let cursorPos = this.canvasToContainer(
-      cursorInfo.x,
-      cursorInfo.y,
-      this.pageCanvasList[pageIndex]
-    )
-    this.setCursor(cursorPos.x, cursorPos.y, cursorInfo.height)
+    this.cursorPositionIndex = positionIndex
+    // 计算光标位置及渲染
+    this.computeAndRenderCursor(positionIndex, pageIndex)
     // 光标测试辅助线
     // let ctx = this.pageCanvasCtxList[pageIndex]
     // ctx.moveTo(cursorInfo.x, cursorInfo.y)
@@ -280,7 +280,7 @@ class CanvasEditor {
       ) {
         // 如果是当前元素的前半部分则点击元素为前一个元素
         if (x < cur.rect.leftTop[0] + cur.info.width / 2) {
-          return i > 0 ? i - 1 : i
+          return i - 1
         }
         return i
       }
@@ -313,22 +313,35 @@ class CanvasEditor {
   // 获取光标位置信息
   getCursorInfo(position) {
     let { fontSize } = this.options
+    // 光标高度在字号的基础上再高一点
+    let height = position.size || fontSize
+    let plusHeight = height / 2
+    let actHeight = height + plusHeight
     // 元素所在行
     let row = this.rows[position.rowIndex]
-    // 光标高度和字号一致，另外再高一点
-    let height = position.size || fontSize
-    height += height / 4
     return {
       x: position.rect.rightTop[0],
       y:
         position.rect.rightTop[1] +
         row.height -
         (row.height - row.originHeight) / 2 -
-        row.descent - // 文本的y坐标
-        height + // 减去光标高度
-        height / 8, // 再减去高出的一半
-      height: height + height / 4
+        actHeight +
+        (actHeight - Math.max(height, position.info.height)) / 2,
+      height: actHeight
     }
+  }
+
+  // 计算光标位置及渲染光标
+  computeAndRenderCursor(positionIndex, pageIndex) {
+    // 根据元素索引计算出光标位置和高度信息
+    let cursorInfo = this.getCursorInfo(this.positionList[positionIndex])
+    // 渲染光标
+    let cursorPos = this.canvasToContainer(
+      cursorInfo.x,
+      cursorInfo.y,
+      this.pageCanvasList[pageIndex]
+    )
+    this.setCursor(cursorPos.x, cursorPos.y, cursorInfo.height)
   }
 
   // 设置光标
@@ -346,6 +359,9 @@ class CanvasEditor {
     this.cursorEl.style.height = height + 'px'
     this.cursorEl.style.opacity = 1
     this.blinkCursor(0)
+    setTimeout(() => {
+      this.focus()
+    }, 0)
   }
 
   // 光标闪烁
@@ -354,6 +370,88 @@ class CanvasEditor {
       this.cursorEl.style.opacity = opacity
       this.blinkCursor(opacity === 0 ? 1 : 0)
     }, 600)
+  }
+
+  // 聚焦
+  focus() {
+    if (!this.textareaEl) {
+      this.textareaEl = document.createElement('textarea')
+      this.textareaEl.style.position = 'fixed'
+      this.textareaEl.style.left = '-99999px'
+      this.textareaEl.addEventListener('input', this.onInput.bind(this))
+      this.textareaEl.addEventListener('compositionstart', () => {
+        this.isCompositing = true
+      })
+      this.textareaEl.addEventListener('compositionend', () => {
+        this.isCompositing = false
+      })
+      this.textareaEl.addEventListener('keydown', this.onKeydown.bind(this))
+      document.body.appendChild(this.textareaEl)
+    }
+    this.textareaEl.focus()
+  }
+
+  // 失焦
+  blur() {
+    if (!this.textareaEl) {
+      return
+    }
+    this.textareaEl.blur()
+  }
+
+  // 输入事件
+  onInput(e) {
+    setTimeout(() => {
+      let data = e.data
+      if (!data || this.isCompositing) {
+        return
+      }
+      // 插入字符
+      let arr = data.split('')
+      let length = arr.length
+      let cur = this.positionList[this.cursorPositionIndex]
+      this.data.splice(
+        this.cursorPositionIndex + 1,
+        0,
+        ...arr.map(item => {
+          return {
+            ...cur,
+            value: item
+          }
+        })
+      )
+      // 重新渲染
+      this.render()
+      // 更新光标
+      this.cursorPositionIndex += length
+      this.computeAndRenderCursor(
+        this.cursorPositionIndex,
+        this.positionList[this.cursorPositionIndex].pageIndex
+      )
+    }, 0)
+  }
+
+  // 按键事件
+  onKeydown(e) {
+    console.log(e)
+    if (e.keyCode === 8) {
+      this.delete()
+    }
+  }
+
+  // 删除
+  delete() {
+    if (this.cursorPositionIndex < 0) {
+      return
+    }
+    this.data.splice(this.cursorPositionIndex, 1)
+    this.render()
+    this.cursorPositionIndex--
+    let position = this.positionList[this.cursorPositionIndex]
+    this.computeAndRenderCursor(
+      this.cursorPositionIndex,
+      position ? position.pageIndex : 0
+    )
   }
 
   // 将相对于浏览器窗口的坐标转换成相对于页面canvas
